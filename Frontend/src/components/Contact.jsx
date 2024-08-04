b31acf3aa6ca8c049e722938ce556c7ee94403f9
Your Name <you@example.com>
2024-08-05 01:15:03 +0530

    deplyment

---
+++ Frontend/src/components/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div>
+      <h1 className='text-5xl text-white '>Contact Us</h1>
+      <br />
+         <p className='text-white'>You can reach us at mishrarakesh2309@gmail.com or call us at 7471175283.</p>
+    </div>
+  )
+}
+
+export default Contact
